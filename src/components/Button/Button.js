0a1ba306a08d29867fbbe24090a55c30007855ec
Enjae Antonio <enjaeantonio@outlook.com>
2023-07-25 13:14:41 -0500

    removed signout component redundancy

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,15 +2,16 @@ import React from "react";
 import './Button.css'
 function Button(props) {
   return (
-		<>
-			<button
-				className={`btn ${props.buttonStyles}`}
-				id={props.id}
-				onClick={props.handleOnClick}>
-				{props.buttonText}
-			</button>
-		</>
-	);
+      <>
+          <button
+              className={`btn ${props.buttonStyles}`}
+              id={props.id}
+              onClick={props.handleOnClick}
+          >
+              {props.buttonText}
+          </button>
+      </>
+  );
 }
 
 export default Button;
--- src/components/ButtonDark/ButtonDark.js
+++ src/components/ButtonDark/ButtonDark.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './ButtonDark.css'
+import './ButtonDark.css';
 
 function ButtonDark(props) {
     return (
         <>
-            <button className={`btn btn-primary btn-dark fw-bold ${props.buttonStyles}`} id={props.id} onClick={props.handleOnClick}>{props.buttonText}</button>
+            <button
+                className={`btn btn-primary btn-dark fw-bold ${props.buttonStyles}`}
+                id={props.id}
+                onClick={props.handleOnClick}
+            >
+                {props.buttonText}
+            </button>
         </>
     );
 }
